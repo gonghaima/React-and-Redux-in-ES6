a6e50ba82742a546694ed1881d9620eb68fe2220
gonghaima@hotmail.com <5418854188>
2016-12-14 19:53:47 +1300

    Initial App Structure

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class AboutPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This app uses React, Redux, React Router and a variaty of other helpful libraries.</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
