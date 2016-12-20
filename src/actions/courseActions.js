3149bd4a251a8a2179eda6356507cf6649ab0184
gonghaima@hotmail.com <5418854188>
2016-12-21 01:56:54 +1300

    Redux Flow - Action Type Constants

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,3 +1,4 @@
-export function createCourse(course){
-    return {type:'CREATE_COURSE', course};
+import * as types from './actionTypes';
+export function createCourse(course) {
+    return { type: types.CREATE_COURSE, course };
 }
