a8da6cdb330d121852c0b4fef2c031989a0b2b0e
gonghaima@hotmail.com <5418854188>
2016-12-25 21:37:29 +1300

    Async in Redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -6,26 +6,8 @@ import * as courseActions from '../../actions/courseActions';
 class CoursesPage extends Component {
     constructor(props, context) {
         super(props, context);
-        this.state = {
-            course: { title: "" }
-        };
 
-        this.onTitleChange=this.onTitleChange.bind(this);
-        this.onClickSave=this.onClickSave.bind(this);
     }
-
-    
-
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({ course: course });
-    }
-
-    onClickSave() {
-        this.props.actions.createCourse(this.state.course);
-    }
-
     courseRow(course, index){
         return <div key={index}>{course.title}</div>;
     }
@@ -35,15 +17,6 @@ class CoursesPage extends Component {
             <div>
                 <h1>Courses</h1>
                 {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input
-                    type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title} />
-                <input
-                    type="submit"
-                    value="Save"
-                    onClick={this.onClickSave} />
             </div>
         );
     }
