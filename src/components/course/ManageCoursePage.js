770c8fc4dfd73a6f7d5b12e224d640b44dcfdf1d
gonghaima@hotmail.com <5418854188>
2016-12-30 00:29:56 +1300

    Async Writes in Redux - Use Manage Course Form

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,30 +2,42 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
     constructor(props, context) {
         super(props, context);
+        this.state = {
+            course: Object.assign({}, this.props.course),
+            errors: {}
+        }
     }
     render() {
         return (
-            <h1>Manage Course</h1>
+
+            <CourseForm
+                allAuthors={[]}
+                course={this.state.course}
+                errors={this.state.errors}
+                />
+
         );
 
     }
 }
 
 ManageCoursePage.PropTypes = {
-    //TODO
+    course: PropTypes.object.isRequired
 }
 
-function mapStateToProps(state, ownProps){
+function mapStateToProps(state, ownProps) {
+    let course = { id: '', watchHref: '', title: '', authorId: '', length: '', category: '' };
     return {
-        state: state
+        course: course
     }
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
     return {
         actions: bindActionCreators(courseActions, dispatch)
     }
