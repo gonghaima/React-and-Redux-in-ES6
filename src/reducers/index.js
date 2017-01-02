3ccfd1389a134ad63e1ebdb92732e801d456f2dc
gonghaima@hotmail.com <5418854188>
2017-01-02 22:32:38 +1300

    Async Status and Error Handling - Create AJAX Status Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxStatusReducer from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
     courses,
-    authors
+    authors,
+    ajaxStatusReducer
 });
 
 export default rootReducer;
