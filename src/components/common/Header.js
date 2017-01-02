2a31500b42824d4cc7284123aaa39772bdb3bbf1
gonghaima@hotmail.com <5418854188>
2017-01-02 22:14:03 +1300

    Async Status and Error Handling - Create Preloader Component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,16 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
+import LoadingDots from './LoadingDots';
 
 const Header = () => {
     return (
         <nav>
             <IndexLink to="/" activeClassName="active">Home</IndexLink>
             {" | "}
-            <IndexLink to="/courses" activeClassName="active">Courses</IndexLink>
+            <Link to="/courses" activeClassName="active">Courses</Link>
             {" | "}
-            <IndexLink to="/about" activeClassName="active">About</IndexLink>
+            <Link to="/about" activeClassName="active">About</Link>
+            <LoadingDots interval={100} dots={20} />
         </nav>
     );
 };
