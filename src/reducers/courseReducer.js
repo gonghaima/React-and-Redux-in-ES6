dbf4b0e52b4696d0277617896dddd4827e7bf2c3
gonghaima@hotmail.com <5418854188>
2017-01-03 22:09:23 +1300

    Bug fix - Courses creating error

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -6,7 +6,8 @@ export default function courseReducer(state = initialState.courses, action) {
         case types.LOAD_COURSES_SUCCESS:
             return action.courses;
         case types.CREATE_COURSE_SUCCESS:
-            return [...state, Object.assign({}, action.course)];
+        debugger;
+            return action.course;
         case types.UPDATE_COURSE_SUCCESS:
             return [
                 ...state.filter(course =>course.id !== action.course.id,
