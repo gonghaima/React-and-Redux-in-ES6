9f0da791daf6dc8fa384ef09fb7468b0a3cfd59b
gonghaima@hotmail.com <5418854188>
2017-01-25 23:11:24 +1300

    Show number of courses in header

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div className="container-fluid">
-                <Header loading={this.props.loading} />
+                <Header loading={this.props.loading} numcourse={this.props.numcourse}/>
                 {this.props.children}
             </div>
         );
@@ -15,12 +15,14 @@ class App extends Component {
 
 App.propTypes = {
     children: PropTypes.object.isRequired,
-    loading: PropTypes.bool.isRequired
+    loading: PropTypes.bool.isRequired,
+    numcourse: PropTypes.number.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
     return {
-        loading: state.ajaxCallsInProgress > 0
+        loading: state.ajaxCallsInProgress > 0,
+        numcourse: state.courses.length
     };
 }
 
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 import LoadingDots from './LoadingDots';
 
-const Header = ({loading}) => {
+const Header = ({loading, numcourse}) => {
     return (
         <nav>
             <IndexLink to="/" activeClassName="active">Home</IndexLink>
@@ -12,6 +12,8 @@ const Header = ({loading}) => {
             <Link to="/authors" activeClassName="active">Authors</Link>
             {" | "}
             <Link to="/about" activeClassName="active">About</Link>
+            {" | "}
+            {numcourse && <span><strong>Total number of courses: {numcourse}</strong></span>}
             {loading && <LoadingDots interval={100} dots={20} />}
         </nav>
     );
